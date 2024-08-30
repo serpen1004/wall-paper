44c8d3a23a63a34f7ed697af6c6cfde1711815e4
serpen1004 <bhargav5g1234@gmail.com>
2024-08-30 19:26:54 +0000

    initialised the app

---
+++ wall-paper/ImmersiveView.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+import RealityKit
+import RealityKitContent
+import ARKit
+
+struct ImmersiveView: View {
+    
+    let session = ARKitSession()
+    let planeData = PlaneDetectionProvider(alignments: [.horizontal, .vertical])
+    
+    @State private var rootEntity = Entity()
+    @State private var planeAnchors: [UUID: PlaneAnchor] = [:]
+    @State private var entityMap: [UUID: Entity] = [:]
+    
+    var body: some View {
+        RealityView { content in
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+                
+                Task {
+                    try await session.run([planeData])
+                    
+                    for await update in planeData.anchorUpdates {
+                        if update.anchor.classification == .window {
+                            continue
+                        }
+                        switch update.event {
+                        case .added, .updated:
+                            await updatePlane(update.anchor)
+                        case .removed:
+                            await removePlane(update.anchor)
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+        
+
+        
+    }
+    
+    @MainActor
+    func updatePlane(_ anchor: PlaneAnchor) async {
+        if planeAnchors[anchor.id] == nil {
+            // Add a new entity to represent this plane.
+            let entity = ModelEntity(mesh: .generateText(anchor.classification.description))
+            entityMap[anchor.id] = entity
+            rootEntity.addChild(entity)
+        }
+        
+        entityMap[anchor.id]?.transform = Transform(matrix: anchor.originFromAnchorTransform)
+        planeAnchors[anchor.id] = anchor
+    }
+    
+    @MainActor
+    func removePlane(_ anchor: PlaneAnchor) async {
+        entityMap[anchor.id]?.removeFromParent()
+        entityMap.removeValue(forKey: anchor.id)
+        planeAnchors.removeValue(forKey: anchor.id)
+    }
+}
+
+#Preview {
+    ImmersiveView()
+        .previewLayout(.sizeThatFits)
+}
+
+
+
